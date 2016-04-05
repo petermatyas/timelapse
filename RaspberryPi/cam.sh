d1b310b07e3a9f352087df9d0dc48881061b3cb6
Peter Matyas <mtysptr@gmail.com>
2016-04-05 21:27:47 +0200

    RPi script improve

--- RaspberryPi/cam.sh
+++ RaspberryPi/cam.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+h=`date +%H`
+
 if [ ! -d /home/pi/camera ]; then
     mkdir -p /home/pi/camera
 fi
 
+if [ $h -ge 4 ] && [ $h -lt 21 ]; then
+  DATE=$(date +"%Y%m%d%H%M%S")
+  raspistill -t 2000 -rot 180 -o /home/pi/camera/$DATE.png -e png
+fi
 
 
 
-DATE=$(date +"%Y%m%d%H%M%S")
-
-raspistill -t 2000 -rot 180 -o /home/pi/camera/$DATE.png -e png
